5c1896827daf138cfd34bc80cb07dbdbc7b1cfac
ando <ando@jimbodenki.co.jp>
2020-10-22 22:37:48 +0900

    Create jp messages resource file

---
+++ volume/lunchbox/resources/lang/ja/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '認証情報が正しくありません。',
+    'throttle' => 'Too many login attempts. 次の試行ができるまで :seconds 秒間お待ちください。',
+
+];
---
+++ volume/lunchbox/resources/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'パスワードをリセットしました。',
+    'sent' => 'パスワードをリセットするための情報を、emailで送信しました。',
+    'throttled' => '少し待ってからもう一度実行してください。',
+    'token' => 'この情報は無効です。パスワードをリセットできません。',
+    'user' => "メールアドレスは見つかりません。",
+
+];
