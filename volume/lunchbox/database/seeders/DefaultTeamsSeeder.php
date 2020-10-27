e48cea24425707f221661276b366061139a05da3
ando <ando@jimbodenki.co.jp>
2020-10-27 23:20:45 +0900

    Teams seed default team set,Default Admin user add

--- volume/lunchbox/database/seeders/DefaultTeamsSeeder.php
+++ volume/lunchbox/database/seeders/DefaultTeamsSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Membership;
-use Illuminate\Database\Seeder;
 use App\Models\Team;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DefaultTeamsSeeder extends Seeder
 {
@@ -16,10 +17,28 @@ class DefaultTeamsSeeder extends Seeder
     public function run()
     {
         Team::insert([
-            ['user_id' => 1, 'name' => '管理者',  'personal_team' => false, 'created_at' => date('Y-m-d H:i:s')],
+            ['user_id' => 1, 'name' => '本社',  'personal_team' => false, 'created_at' => date('Y-m-d H:i:s')],
+            ['user_id' => 2, 'name' => '支社',  'personal_team' => false, 'created_at' => date('Y-m-d H:i:s')],
         ]);
-        Membership::insert([
-            ['user_id' => 1, 'team_id' => 1, 'role' => 'admin', 'created_at' => date('Y-m-d H:i:s')],
+        User::insert([
+            ['id' => 1,
+                'name' => '本社 admin',
+                'email' => 'admin1@exmple.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+                'current_team_id' => 1,
+                'created_at' => date('Y-m-d H:i:s'),
+            ],
+            ['id' => 2,
+                'name' => '支社 admin',
+                'email' => 'admin2@exmple.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+                'current_team_id' => 2,
+                'created_at' => date('Y-m-d H:i:s'),
+            ],
         ]);
     }
 }
