4ac8fb5ec5ed26b10a665e60f49b2fecbf11b4c3
ando <ando@jimbodenki.co.jp>
2020-10-28 23:10:51 +0900

    Change button design tailwind.css

--- volume/lunchbox/resources/js/Pages/Order/Index.vue
+++ volume/lunchbox/resources/js/Pages/Order/Index.vue
@@ -137,9 +137,9 @@
       },
       buttonClass: function(id) {
         if (id == this.active_plan_id) {
-          return 'btn btn-primary mr-3';
+          return 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded';
         }
-        return 'btn btn-outline-secondary mr-3';
+        return 'bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded';
       },
       formatShortDate: function(value) {
         if (!value) return '';
